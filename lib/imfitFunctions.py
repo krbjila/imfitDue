0a3e16a0cfa4cd6635cdaebaab463be94fa791c7
KRb <krb.jila@gmail.com>
2024-11-22 11:37:16 -0700

    fix power implementation in Thomas-Fermi fit

--- lib/imfitFunctions.py
+++ lib/imfitFunctions.py
@@ -152,9 +152,12 @@ def thomasFermi(p, r, y):
     return np.ravel(
         p[0]
         + p[1]
-        * np.maximum(
-            # 3/2 power for integrated profile
-            (1 - (X - p[2]) ** 2.0 / p[3] ** 2.0 - (Y - p[4]) ** 2.0 / p[5] ** 2.0)**(1.5), 0
+        # 3/2 power for integrated profile
+        * np.power(
+            np.maximum(
+                1 - (X - p[2]) ** 2.0 / p[3] ** 2.0 - (Y - p[4]) ** 2.0 / p[5] ** 2.0, 0
+            ),
+            3 / 2,
         )
         + p[6]
         * np.exp(
